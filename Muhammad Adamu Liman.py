e3e69d043685196d2053b1710fed75c39bfce2a5
Code-Liman <81833194+Code-Liman@users.noreply.github.com>
2021-04-04 19:57:48 +0100

    Add files via upload

---
+++ Muhammad Adamu Liman.py
@@ -0,0 +1,25 @@
+print("NAME:Muhammad adamu liman")
+print("DEPARTMENT: Computer Science")
+scare = "Boo!"
+print (scare)
+var1 = 28
+var2 = 75
+third_score= var1 + var2
+print(var1, var2, third_score)
+var5 = 12
+var4 = .5
+var6 = var5 / var4
+print (var6)
+one = 70
+two = 40
+three = one + two
+print (three)
+dash = 8
+crash = dash % 3
+print (crash)
+A1 = 2
+A2 = A1 * 4
+B1 = 4
+B2 = B1 + 2
+print (A2 * B1 + 2)
+
